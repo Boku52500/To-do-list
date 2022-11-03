c2efdc2f5b40f3248b749d5f0fe82a5587286d11
Giorgi Bokuchava <113925720+Boku52500@users.noreply.github.com>
2022-11-03 11:10:42 +0400

    Created add function

--- modules/actions.js
+++ modules/actions.js
@@ -1,52 +1,38 @@
 // eslint-disable-next-line  no-unused-vars
+
+import { add } from "lodash"
 const data = JSON.parse(localStorage.getItem('todo-list'));
-const list = document.querySelector('.list');
-const tasks = [
-  {
-    description: 'Finishing project',
-    completed: false,
-    index: 0,
-  }, {
-    description: 'Learning tasks',
-    completed: false,
-    index: 1,
-  }, {
-    description: 'Approving project',
-    completed: false,
-    index: 2,
-  },
-];
+const input = document.querySelector('.entered-list')
+const addBtn = document.querySelector('.add-list')
+const tasks = document.querySelector('.tasks')
 
-export default class Actions {
-  static showTask(data) {
-    let li = '';
-    if (data) {
-      data.forEach((toDo, id) => {
-        li += `<li class='task'>
-      <input type='checkbox' id='${id}'>
-      <p>${toDo.description}</p>
-      <img class="dots" src="91b1f318fbc3ef6f0456.png" alt="3 dots" style="cursor:pointer"/>
-      </li>
-      `;
-      });
-    }
-    list.innerHTML = li;
+export default input.addEventListener('keyup', () => {
+  if(input.value.trim() != 0){
+    addBtn.classList.add('active')
+  } else {
+    addBtn.classList.remove('active')
   }
+})
 
-  static addElem() {
-    tasks.forEach(() => {
-      let li = '';
-      if (tasks) {
-        tasks.forEach((toDo, id) => {
-          li += `<li class='task'>
-        <input type='checkbox' id='${id}'>
-        <p>${toDo.description}</p>
-        <img class="dots" src="91b1f318fbc3ef6f0456.png" alt="3 dots" style="cursor:pointer"/>
-        </li>
-        `;
-        });
-      }
-      list.innerHTML = li;
-    });
+addBtn.addEventListener('click', () => {
+  if(input.value.trim() != 0){
+    const newItem = document.createElement('div');
+    newItem.classList.add('list-item')
+    newItem.innerHTML = `<li class='task'>
+    <input type='checkbox' id='checkBox'>
+    <p>${input.value}</p>
+    <img class="dots" src="91b1f318fbc3ef6f0456.png" alt="3 dots" style="cursor:pointer"/>
+    </li>`
+
+    tasks.appendChild(newItem);
+    input.value = '';
+  } else {
+    alert('empty')
   }
-}
+})
+
+tasks.addEventListener('click', (e) => {
+  if(e.target.checked) {
+    
+  }
+})
